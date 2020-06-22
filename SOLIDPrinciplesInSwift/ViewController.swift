7d246fbe02d4eb5b842797d70d5c794e7342ce41
Shiva Kumar <shiva.kutt.kumar@gmail.com>
2020-06-22 23:48:41 +0800

    Initial commit

---
+++ SOLIDPrinciplesInSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOLIDPrinciplesInSwift
+//
+//  Created by shiva kumar on 22/6/20.
+//  Copyright © 2020 shiva kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
